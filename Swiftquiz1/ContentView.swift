b68d63b2d421994984c04dbf18a66e7998aac976
dd <dd>
2022-12-07 23:40:01 -0800

    Initial Commit

---
+++ Swiftquiz1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftquiz1
+//
+//  Created by kourosh on 16/09/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
